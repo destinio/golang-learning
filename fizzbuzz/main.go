07aaac393fd3a1d3b376362408527cd28c8d6a88
Destin Lee <destin.lee@johnsonfit.com>
2022-08-10 14:23:56 -0500

    time switch

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -2,15 +2,54 @@ package main
 
 import "fmt"
 
-func fizzBuzz() {
+func fizzBuzzSwitch() {
 	for i := 1; i <= 40; i++ {
 		fizz := "Fizz"
 		buzz := "Buzz"
 
-		if i % 3 == 0 && i % 5 == 0 { fmt.Println(i, fizz + buzz) }
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, fizz+buzz)
+		}
 
-		if i % 3 == 0 { fmt.Println(i, fizz) }
-		if i % 5 == 0 { fmt.Println(i, buzz) }
+		if i%3 == 0 {
+			fmt.Println(i, fizz)
+		}
+		if i%5 == 0 {
+			fmt.Println(i, buzz)
+		}
+
+		switch {
+		case (i%3 == 0) && (i%5 == 0):
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+
+		}
+
+		fmt.Println(i)
+
+	}
+}
+
+func fizzBuzzIf() {
+	for i := 1; i <= 40; i++ {
+		fizz := "Fizz"
+		buzz := "Buzz"
+
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, fizz+buzz)
+		}
+
+		if i%3 == 0 {
+			fmt.Println(i, fizz)
+		}
+		if i%5 == 0 {
+			fmt.Println(i, buzz)
+		}
 
 		fmt.Println(i)
 
@@ -18,5 +57,6 @@ func fizzBuzz() {
 }
 
 func main() {
-	fizzBuzz()
+	fizzBuzzIf()
+	fizzBuzzSwitch()
 }
---
+++ timeSwitch/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func isDay() {
+	day := time.Now().Weekday()
+
+
+	fmt.Println(time.Tuesday)
+
+	switch time.Friday {
+	case day + 0:
+		fmt.Println("Today.")
+	case day + 1:
+		fmt.Println("one day left")
+	case day + 2:
+		fmt.Println("Two days left")
+	default:
+		fmt.Println("A ways to go man!!")
+	}
+}
+
+func main() {
+	isDay()
+}
